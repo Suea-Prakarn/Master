052f1c25396003df29cf0f5a1e4c5ec06b709390
Suea-Prakarn <Prakarn.Pholkhamtong@gmail.com>
2025-01-03 17:32:09 +0700

    ีmaster

---
+++ Mywebsite/products.php
@@ -0,0 +1,69 @@
+<?php
+// ข้อมูลสินค้า (ตัวอย่าง)
+$products = [
+  [
+    'image' => 'assets/1.png',
+    'name' => 'Art1',
+    'price' => '#',
+    'discription' => 'Art1 is a beautiful art',
+    'rating' => 4,
+    'sale' => false,
+  ],
+  [
+    'image' => 'assets/1.png',
+    'name' => 'Art1',
+    'price' => '#',
+    'discription' => 'Art1 is a beautiful art',
+    'rating' => 4,
+    'sale' => false,
+  ],      
+  [
+    'image' => 'assets/1.png',
+    'name' => 'Art1',
+    'price' => '#',
+    'discription' => 'Art1 is a beautiful art',
+    'rating' => 4,
+    'sale' => false,
+  ],      
+  [
+    'image' => 'assets/1.png',
+    'name' => 'Art1',
+    'price' => '#',
+    'discription' => 'Art1 is a beautiful art',
+    'rating' => 4,
+    'sale' => false,
+  ],      
+  [
+    'image' => 'assets/1.png',
+    'name' => 'Art1',
+    'price' => '#',
+    'discription' => 'Art1 is a beautiful art',
+    'rating' => 4,
+    'sale' => false,
+  ],      
+  [
+    'image' => 'assets/1.png',
+    'name' => 'Art1',
+    'price' => '#',
+    'discription' => 'Art1 is a beautiful art',
+    'rating' => 4,
+    'sale' => false,
+  ],      
+  [
+    'image' => 'assets/1.png',
+    'name' => 'Art1',
+    'price' => '#',
+    'discription' => 'Art1 is a beautiful art',
+    'rating' => 4,
+    'sale' => false,
+  ],      
+  [
+    'image' => 'assets/1.png',
+    'name' => 'Art1',
+    'price' => '#',
+    'discription' => 'Art1 is a beautiful art',
+    'rating' => 4,
+    'sale' => false,
+  ]     
+];
+?>
